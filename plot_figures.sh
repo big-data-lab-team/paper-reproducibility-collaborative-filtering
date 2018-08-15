9b374e83f5b8ce146c669387d7bdc69be27ccd8e
Soudabeh Barghi <binrepro@cedar1.cedar.computecanada.ca>
2018-08-15 15:11:44 -0700

    Added the plot script of user/item factors figures

--- plot_figures.sh
+++ plot_figures.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in ./data/results/means_of_results/ALS-Bias/Synthetic/synthetic_subject_types/plot.gnplt ./data/results/means_of_results/ALS-Bias/FS-100files/plot.gnplt ./data/results/means_of_results/ALS-Bias/PreFreeSurfer/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/synthetic_subject_types/plot.gnplt ./data/results/means_of_results/ALS/FS-100files/plot.gnplt ./data/results/means_of_results/ALS/PreFreeSurfer/plot.gnplt ./data/results/roc/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/8type-RFNU-Differ-Num-Factors/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/8type-RFNU-Differ-Num-Iterations-rank50/plot-iteration.gnplt
+for file in ./data/results/means_of_results/ALS-Bias/Synthetic/synthetic_subject_types/plot.gnplt ./data/results/means_of_results/ALS-Bias/FS-100files/plot.gnplt ./data/results/means_of_results/ALS-Bias/PreFreeSurfer/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/synthetic_subject_types/plot.gnplt ./data/results/means_of_results/ALS/FS-100files/plot.gnplt ./data/results/means_of_results/ALS/PreFreeSurfer/plot.gnplt ./data/results/roc/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/8type-RFNU-Differ-Num-Factors/plot.gnplt ./data/results/means_of_results/ALS/Synthetic/8type-RFNU-Differ-Num-Iterations-rank50/plot-iteration.gnplt ./data/results/latent-factors/itemFactor-plot-3d.gnplt  ./data/results/latent-factors/userFactor-plot-3d.gnplt
 do
   dir=$(dirname $file)
   base=$(basename $file)
